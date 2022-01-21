d5b939cfd7a423b6fe6dfc70066fc3abc888c742
Thomas Ngo <thomasngo818@gmail.com>
2022-01-21 02:31:21 +0000

    added functions to get ip and to get coordinates based on ip

---
+++ index.js
@@ -0,0 +1,15 @@
+const { fetchMyIP, fetchCoordsByIp } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+// fetchCoordsByIp('75.157.109.252', (error, data) => {
+  // console.log(error);
+  // console.log(data);
+// });
---
+++ iss.js
@@ -0,0 +1,65 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const data = JSON.parse(body);
+    callback(null, data.ip);
+  });
+};
+
+// Makes api request to retrive location
+// takes an ip address and a callback
+// returns (through the callback)
+//    an error if any
+//    an object containing properties for both latitude and longitude
+const fetchCoordsByIp = function(ip, callback) {
+  // console.log('ip', ip);
+  request(`https://api.freegeoip.app/json/${ip}?apikey=1599f620-7a56-11ec-be5a-69af916c679f`,
+    (error, response, body) => {
+      const coords = {};
+
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      const data = JSON.parse(body);
+      coords.latitude = data.latitude;
+      coords.longitude = data.longitude;
+      callback(null, coords);
+    });
+};
+
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+};
+module.exports = { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes };
